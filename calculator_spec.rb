9b28631bc19a7a211e8b544543ae9f60d4d35d93
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-10-31 12:04:21 +0000

    calculator will now square a number

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -1,10 +1,13 @@
 require 'calculator.rb'
 
 describe Calculator do
-  it 'multiplies two numbers using #multiply' do
-    expect(subject).to respond_to(:multiply)
 
   it 'multiplies two integers' do
     expect(subject(1, 1)).to_eq(1)
   end
+
+  it 'will square a number using #square' do
+    expect(subject(2)).to_eq(4)
+  end
+
 end
--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,11 @@
 class Calculator
+
   def multiply(a, b)
     (a * b)
+  end
+
+  def square(n)
+    (n * n)
   end
 
 end
